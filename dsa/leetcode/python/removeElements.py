2ec6c37ea5cd8c0f861828f13d55ba0d613b59f5
raitososavage <raitohoacelonmuskvn@gmail.com>
2025-12-14 18:43:16 +0700

    two pointers: removeElements

---
+++ dsa/leetcode/python/removeElements.py
@@ -0,0 +1,21 @@
+#method 1: O(n)
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n-1]
+                n -= 1
+            else:
+                i += 1
+        return n
+#method 2: o(n) permanent order
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for x in nums:
+            if x != val:
+                nums[k] = x
+                k += 1
+        return k
